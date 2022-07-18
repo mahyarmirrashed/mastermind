97a2e0ca3918aaa0f23c6fef306225350b6e4a01
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-07-18 18:47:00 -0500

    feat: add constructor for feedback structure
    Feedback structure can now be constructed from an array of color code pegs representing the current
    guess and another array of color code pegs representing the answer.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 /// Color code pegs for hole guessing (will be colored)
 const COLOR_PEG: &str = "\u{2b24}";
 /// White key peg for correct color code peg placed in wrong position
@@ -6,8 +8,8 @@ const RCWP_PEG: &str = "\u{25cf}";
 const RCRP_PEG: &str = "\u{25e6}";
 
 /// Subset of the standard eight ANSI colors
-#[derive(Eq, Hash, PartialEq)]
-enum Color {
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+pub enum Color {
     Red,
     Green,
     Yellow,
@@ -18,10 +20,54 @@ enum Color {
 }
 
 /// Feedback provided to codebreaker by codemaker
-struct Feedback {
+pub struct Feedback {
     /// Number of correct color code pegs in wrong position
     wrong: u8,
 
-    /// Number of correct color code pegs in correct position
-    correct: u8,
+    /// Number of correct color code pegs in right position
+    right: u8,
+}
+
+impl Feedback {
+    /// Creates a new feedback structure based on the frequency of color code pegs in the guess and answer.
+    pub fn new(guess: &[Color], answer: &[Color]) -> Result<Feedback, &'static str> {
+        // preconditions
+        debug_assert!(guess.len() > 0);
+        debug_assert!(guess.len() == answer.len());
+
+        if guess.len() == 0 {
+            return Err("Guess length was zero.");
+        } else if guess.len() != answer.len() {
+            return Err("Guess and answer length were not equal.");
+        }
+
+        // frequency hashmap to store frequencies of answer values
+        let mut frequencies: HashMap<Color, u8> = HashMap::new();
+        // feedback parameters
+        let mut wrong: u8 = 0;
+        let mut right: u8 = 0;
+
+        // convert answer list into frequency hashmap
+        for color in answer {
+            *frequencies.entry(*color).or_insert(0) += 1;
+        }
+
+        // count number of incorrect color code pegs
+        for color in guess {
+            if let Some(frequency) = frequencies.get_mut(color) {
+                *frequency -= 1;
+                wrong += 1;
+            }
+        }
+
+        // rebalance and count number of correct color code pegs
+        for peg in guess.iter().zip(answer) {
+            if peg.0 == peg.1 {
+                wrong -= 1;
+                right += 1;
+            }
+        }
+
+        Ok(Feedback { wrong, right })
+    }
 }
