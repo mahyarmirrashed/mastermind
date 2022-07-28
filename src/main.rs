d46565a8386ea50d0de27d4151339f497a02f5b3
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-07-28 18:14:24 -0500

    refactor: create separate function for displaying guess history

--- src/main.rs
+++ src/main.rs
@@ -40,27 +40,10 @@ fn main() {
     // track number of guesses made
     let mut guesses = 0usize;
 
-    // enter into raw mode terminal parsing
-    let mut stdout = stdout().into_raw_mode().unwrap();
-
     // loop all through all guesses until exhausted all turns or guessed correctly
     while guesses < turns {
-        // clear entire terminal output
-        write!(
-            stdout,
-            "{}{}",
-            termion::clear::All,
-            termion::cursor::Goto(1, 1)
-        )
-        .expect("Not written.");
-        // flush output
-        stdout.flush().expect("Unable to flush standard output!");
-
-        // print guess history along with feedback
-        for i in 0..guesses {
-            let guess = &history[(i * pegs)..((i + 1) * pegs)].iter().join("  ");
-            write!(stdout, "[ {} ] ( {} )\r\n", guess, feedback[guesses]).expect("Not written.");
-        }
+        // display history to user
+        display(&history, &answer, pegs, guesses);
 
         // create vector holding user guesses
         let mut guess = vec![ColorPeg::White; pegs];
@@ -80,7 +63,8 @@ fn main() {
         }
 
         // save guess into past history
-        history[(guesses * pegs)..((guesses + 1) * pegs)].copy_from_slice(&guess);
+        let offset = guesses * pegs;
+        history[offset..(offset + guess.len())].copy_from_slice(&guess);
         // calculate feedback based on current guess
         feedback[guesses] = Feedback::new(&guess, &answer).expect("Unable to create feedback.");
 
@@ -90,5 +74,33 @@ fn main() {
         }
 
         guesses += 1;
+    }
+}
+
+/// Display guess history to user.
+fn display(history: &Vec<ColorPeg>, answer: &Vec<ColorPeg>, pegs: usize, guesses: usize) {
+    // create raw mode standard output
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    // clear terminal output and place cursor in (1,1)
+    write!(
+        stdout,
+        "{}{}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1)
+    )
+    .expect("Not written.");
+    // flush output
+    stdout.flush().expect("Unable to flush standard output!");
+
+    // print guess history along with feedback
+    for guess in history[..guesses * pegs].chunks(pegs) {
+        write!(
+            stdout,
+            "[ {} ] ( {} )\r\n",
+            guess.iter().join("  "),
+            Feedback::new(&guess, &answer).unwrap_or(Feedback { wrong: 0, right: 0 })
+        )
+        .expect("Not written.");
     }
 }
