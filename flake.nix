30a94bef6cf97c741bda94c2de7ec76a45026697
Mahyar Mirrashed <mah.mirr@gmail.com>
2026-03-11 14:39:58 -0500

    flake.nix,flake.lock: init

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Flake for github:mahyarmirrashed/mastermind";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+          ];
+        };
+      }
+    );
+}
