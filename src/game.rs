8877860df2ec41b08452d9ca15ac29d08c317dd0
Mahyar Mirrashed <mah.mirr@gmail.com>
2026-03-11 15:53:10 -0500

    src/{game,main}.rs: move game logic into own file

---
+++ src/game.rs
@@ -0,0 +1,131 @@
+use std::io::{stdin, stdout, Stdout, Write};
+
+use itertools::Itertools;
+use rand::{distributions, Rng};
+use termion::{
+    event::Key,
+    input::TermRead,
+    raw::{IntoRawMode, RawTerminal},
+};
+
+use mastermind::{ColorPeg, Feedback};
+
+const INSTRUCTIONS: &str = "╔══════════════════════════════════╗\r\n\
+                            ╟----------- Mastermind -----------╢\r\n\
+                            ╠══════════════════════════════════╣\r\n\
+                            ║ ◀ ▶ | move cursor left and right ║\r\n\
+                            ║ ▼ ▲ | shuffle through colors     ║\r\n\
+                            ║ ↵   | submit guess               ║\r\n\
+                            ║ q   | quit the game              ║\r\n\
+                            ╚══════════════════════════════════╝\r\n\
+                            ";
+
+pub struct Game {
+    stdout: RawTerminal<Stdout>,
+    colorblind: bool,
+    pegs: usize,
+    guesses: usize,
+    answer: Vec<ColorPeg>,
+    history: Vec<ColorPeg>,
+    guess_count: usize,
+}
+
+impl Game {
+    pub fn new(pegs: usize, guesses: usize, colorblind: bool) -> Self {
+        let answer = rand::thread_rng()
+            .sample_iter(distributions::Standard)
+            .take(pegs)
+            .collect();
+
+        Self {
+            stdout: stdout().into_raw_mode().unwrap(),
+            colorblind,
+            pegs,
+            guesses,
+            answer,
+            history: vec![ColorPeg::White; pegs * guesses],
+            guess_count: 0,
+        }
+    }
+
+    pub fn run(&mut self) {
+        while self.guess_count < self.guesses {
+            let mut guess = vec![ColorPeg::White; self.pegs];
+            let mut cursor = 0;
+
+            self.display(Some(&guess), Some(cursor));
+
+            for chr in stdin().keys() {
+                match chr.unwrap() {
+                    Key::Up => guess[cursor] = guess[cursor].up(),
+                    Key::Down => guess[cursor] = guess[cursor].down(),
+                    Key::Left => cursor = (cursor + self.pegs - 1) % self.pegs,
+                    Key::Right => cursor = (cursor + self.pegs + 1) % self.pegs,
+                    Key::Char('\n' | ' ') => break,
+                    Key::Char('q') | Key::Ctrl('c' | 'd') => return,
+                    _ => {}
+                }
+                self.display(Some(&guess), Some(cursor));
+            }
+
+            let offset = self.guess_count * self.pegs;
+            self.history[offset..offset + self.pegs].copy_from_slice(&guess);
+            self.guess_count += 1;
+
+            if Feedback::new(&guess, &self.answer).unwrap().right == self.pegs {
+                self.display(None, None);
+                write!(self.stdout, "Congratulations, you won!\r\n").expect("Not written.");
+                return;
+            }
+        }
+
+        self.display(None, None);
+        write!(self.stdout, "Sorry, you lost!\r\n").expect("Not written.");
+    }
+
+    fn display(&mut self, guess: Option<&[ColorPeg]>, guess_cursor: Option<usize>) {
+        write!(
+            self.stdout,
+            "{}{}{}\r\n",
+            termion::clear::All,
+            termion::cursor::Goto(1, 1),
+            INSTRUCTIONS
+        )
+        .expect("Not written.");
+        self.stdout
+            .flush()
+            .expect("Unable to flush standard output!");
+
+        let history_rows = &self.history[..self.guess_count * self.pegs];
+        let has_history = !history_rows.is_empty();
+
+        for (i, row) in history_rows.chunks(self.pegs).enumerate() {
+            write!(
+                self.stdout,
+                "Guess {:0>2}: [ {} ] {}\r\n",
+                i + 1,
+                row.iter().map(|p| p.display(self.colorblind)).join(" "),
+                Feedback::new(row, &self.answer)
+                    .unwrap_or_default()
+                    .display(self.colorblind)
+            )
+            .expect("Not written.");
+        }
+
+        if has_history {
+            write!(self.stdout, "\r\n").expect("Not written.");
+        }
+
+        if let Some(guess) = guess {
+            write!(
+                self.stdout,
+                "[ {} ]\r\n",
+                guess.iter().map(|p| p.display(self.colorblind)).join(" ")
+            )
+            .expect("Not written.");
+            let mut cursor_row = vec![' '; guess.len()];
+            cursor_row[guess_cursor.unwrap_or_default()] = '^';
+            write!(self.stdout, "  {}  \r\n", cursor_row.iter().join(" ")).expect("Not written.");
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,10 @@
 #![forbid(unsafe_code)]
 
-use std::io::{stdin, stdout, Stdout, Write};
+mod game;
 
 use clap::Parser;
-use itertools::Itertools;
-use rand::{distributions, Rng};
-use termion::{
-    event::Key,
-    input::TermRead,
-    raw::{IntoRawMode, RawTerminal},
-};
 
-use mastermind::{ColorPeg, Feedback};
-
-const INSTRUCTIONS: &str = "╔══════════════════════════════════╗\r\n\
-                            ╟----------- Mastermind -----------╢\r\n\
-                            ╠══════════════════════════════════╣\r\n\
-                            ║ ◀ ▶ | move cursor left and right ║\r\n\
-                            ║ ▼ ▲ | shuffle through colors     ║\r\n\
-                            ║ ↵   | submit guess               ║\r\n\
-                            ║ q   | quit the game              ║\r\n\
-                            ╚══════════════════════════════════╝\r\n\
-                            ";
-
+use game::Game;
 
 #[derive(Parser, Debug)]
 /// Mastermind is a game where the codebreaker tries to guess the pattern in
@@ -52,159 +34,13 @@ struct Args {
 
     /// Replace colors with numbers and feedback symbols for colorblind accessibility.
     /// Also activated by setting the NO_COLOR environment variable.
-    #[clap(short = 'c', long)]
+    #[clap(short, long)]
     colorblind: bool,
 }
 
-/// Program enters here. Main logic is performed here until game completes.
 fn main() {
-    // parse arguments pass to program
     let args = Args::parse();
-    let pegs = args.pegs as usize;
-    let guesses = args.guesses as usize;
     let colorblind = args.colorblind || std::env::var_os("NO_COLOR").is_some();
 
-    // generate code (answer) needing to be guessed by player
-    let answer: Vec<ColorPeg> = rand::thread_rng()
-        .sample_iter(distributions::Standard)
-        .take(pegs)
-        .collect();
-
-    // track guess history and guess count
-    let mut guess_history = vec![ColorPeg::White; pegs * guesses];
-    let mut guess_count = 0;
-    let mut won = false;
-
-    // create raw mode standard output
-    let mut stdout = stdout().into_raw_mode().unwrap();
-
-    // continue until player guesses correctly or runs out of guesses
-    while guess_count < guesses {
-        // track current player guess and cursor location
-        let mut guess = vec![ColorPeg::White; pegs];
-        let mut guess_cursor = 0;
-
-        // display terminal user interface
-        display(
-            guess_history[..guess_count * pegs].chunks(pegs),
-            &answer,
-            Some(&guess),
-            Some(guess_cursor),
-            colorblind,
-            &mut stdout,
-        );
-
-        // process based on keystroke
-        for chr in stdin().keys() {
-            // handle next character from standard input
-            match chr.unwrap() {
-                Key::Up => guess[guess_cursor] = guess[guess_cursor].up(),
-                Key::Down => guess[guess_cursor] = guess[guess_cursor].down(),
-                Key::Left => guess_cursor = (guess_cursor + pegs - 1) % pegs,
-                Key::Right => guess_cursor = (guess_cursor + pegs + 1) % pegs,
-                Key::Char('\n' | ' ') => break,
-                Key::Char('q') | Key::Ctrl('c' | 'd') => return,
-                _ => {}
-            }
-
-            // re-render terminal user interface with new guess
-            display(
-                guess_history[..guess_count * pegs].chunks(pegs),
-                &answer,
-                Some(&guess),
-                Some(guess_cursor),
-                colorblind,
-                &mut stdout,
-            );
-        }
-
-        // save guess into guess history
-        let offset = guess_count * pegs;
-        guess_history[offset..offset + guess.len()].copy_from_slice(&guess);
-
-        // increase guess count
-        guess_count += 1;
-
-        // quick escape if guess was correct
-        if Feedback::new(&guess, &answer).unwrap().right == pegs {
-            won = true;
-            break;
-        }
-    }
-
-    // display final game output without guess prompt
-    display(
-        guess_history[..guess_count * pegs].chunks(pegs),
-        &answer,
-        None,
-        None,
-        colorblind,
-        &mut stdout,
-    );
-
-    // display output based on win or loss
-    if won {
-        write!(stdout, "Congratulations, you won!\r\n").expect("Not written.");
-    } else {
-        write!(stdout, "Sorry, you lost!\r\n").expect("Not written.");
-    }
-}
-
-/// Display terminal user interface to user.
-fn display(
-    history: std::slice::Chunks<ColorPeg>,
-    answer: &[ColorPeg],
-    guess: Option<&[ColorPeg]>,
-    guess_cursor: Option<usize>,
-    colorblind: bool,
-    stdout: &mut RawTerminal<Stdout>,
-) {
-    // clear terminal output, place cursor in (1, 1), and print instructions
-    write!(
-        stdout,
-        "{}{}{}\r\n",
-        termion::clear::All,
-        termion::cursor::Goto(1, 1),
-        INSTRUCTIONS
-    )
-    .expect("Not written.");
-    // flush output, clearing terminal is often buffered
-    stdout.flush().expect("Unable to flush standard output!");
-
-    // determine whether to include guess separator
-    let separate_guesses = history.len() != 0;
-
-    // print guess history along with feedback
-    for (i, row) in history.enumerate() {
-        write!(
-            stdout,
-            "Guess {:0>2}: [ {} ] {}\r\n",
-            i + 1,
-            row.iter().map(|p| p.display(colorblind)).join(" "),
-            Feedback::new(row, answer)
-                .unwrap_or_default()
-                .display(colorblind)
-        )
-        .expect("Not written.");
-    }
-
-    // print newline to separate from guesses
-    if separate_guesses {
-        write!(stdout, "\r\n").expect("Not written.");
-    }
-
-    // print current guess, if necessary
-    if let Some(guess) = guess {
-        // current guess selected by user
-        write!(
-            stdout,
-            "[ {} ]\r\n",
-            guess.iter().map(|p| p.display(colorblind)).join(" ")
-        )
-        .expect("Not written.");
-        // current cursor location on guess
-        let mut cursor = vec![' '; guess.len()];
-        cursor[guess_cursor.unwrap_or_default()] = '^';
-        write!(stdout, "  {}  \r\n", cursor.iter().join(" ")).expect("Not written.");
-    }
+    Game::new(args.pegs as usize, args.guesses as usize, colorblind).run();
 }
